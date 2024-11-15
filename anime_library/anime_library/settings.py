5fc6b3e23b33e701c604d6dc5a3f841fb763a0fd
duynguyen <duyntt0110@gmail.com>
2024-11-15 16:20:46 +0700

    config setting

--- anime_library/anime_library/settings.py
+++ anime_library/anime_library/settings.py
@@ -57,16 +57,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost',
+    'http://172.19.85.52',
+    'http://localhost'
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost',
+    'http://172.19.85.52',
+    'http://localhost'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
+    'http://172.19.85.52',
+    'http://localhost'
 ]
 
 
