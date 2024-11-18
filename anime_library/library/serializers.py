09593100fcc203bd2ebb63ab210170ac009990d0
duynguyen <duyntt0110@gmail.com>
2024-11-18 16:58:01 +0700

    serializer

--- anime_library/library/serializers.py
+++ anime_library/library/serializers.py
@@ -6,10 +6,30 @@ class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = ('id', 'title', 'channel', 'views', 'posted_at', 'duration', 'thumbnail_url', 'video_url')
-        read_only_fields = ('id', 'posted_at', 'duration')
+        read_only_fields = ('id', 'posted_at', 'duration', 'channel')
+
+    def create(self, validated_data):
+        user_video_data = {}
+
+        video_record = Video.objects.create(**user_video_data)
+
+        return video_record
+
+    def update(self, instance, validated_data):
+        pass
 
 class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Channel
         fields = ('id', 'channel_name', 'profile_url')
         read_only_fields = ('id', 'channel_name', 'created_at')
+
+    def create(self, validated_data):
+        user_channel_data = {}
+
+        channel_record = Channel.objects.create(**user_channel_data)
+
+        return channel_record
+
+    def update(self, instance, validated_data):
+        pass
